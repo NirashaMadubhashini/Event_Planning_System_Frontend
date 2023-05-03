e32325a89f3dfcc798cfb690c44e45321f9dfe77
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-05-03 12:37:06 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import './index.css';
+import Navbar from "./components/navbar/Navbar";
+import SignUp from "./components/Auth/Auth";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Container maxWidth="xl">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={() => <Redirect to="/auth" />} />
+                    <Route path="/auth" exact component={SignUp} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+
+    );
+};
+
+export default App;
