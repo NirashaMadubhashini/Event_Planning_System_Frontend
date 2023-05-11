9dac4f59b26fe332f6cacd89bf9d42bf61cca339
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-05-11 12:09:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import Home from "./components/Home/Home";
 import Service from "./components/Services/Service";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 
 
@@ -17,9 +18,10 @@ const App = () => {
                 <Navbar />
                 <Switch>
                     <Route path="/" exact component={() => <Redirect to="/auth" />} />
+                    <Route path="/auth" exact component={SignUp} />
                     <Route path="/home" exact component={Home} />
                     <Route path="/service" exact component={Service} />
-                    <Route path="/auth" exact component={SignUp} />
+                    <Route path="/dashboard" exact component={Dashboard} />
                 </Switch>
             </Container>
         </BrowserRouter>
