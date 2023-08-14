61669ad8932b49dda0d2d6c4d8d3882316537e5f
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-08-14 12:25:36 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
-// import { combineReducers } from 'redux';
-//
-// import posts from './posts';
-// import auth from './auth';
-//
-// export const reducers = combineReducers({ posts, auth });
+import { combineReducers } from 'redux';
+
+import adminReducer from './adminReducer';
+
+export const reducers = combineReducers({ adminReducer });
