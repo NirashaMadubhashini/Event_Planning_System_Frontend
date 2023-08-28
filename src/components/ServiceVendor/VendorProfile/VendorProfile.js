f660e43e44e4415137fb0ca295761e4c089c14f2
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-08-28 11:07:55 +0530

    Update vendor VendorProfile.js

--- src/components/ServiceVendor/VendorProfile/VendorProfile.js
+++ src/components/ServiceVendor/VendorProfile/VendorProfile.js
@@ -127,17 +127,6 @@ const VendorProfile = () => {
                             </Typography>
                             <Typography
                                 component={Link}
-                                to="/vendorAddBusiness"
-                                className={classes.appBarButton}
-                                onClick={() => handleCategoryClick("Business")}
-                                style={{
-                                    color: clickedCategory === "Business" ? "#F50057" : "",
-                                }}
-                            >
-                                Business
-                            </Typography>
-                            <Typography
-                                component={Link}
                                 to="/vendorAddPackages"
                                 className={classes.appBarButton}
                                 onClick={() => handleCategoryClick("Packages")}
