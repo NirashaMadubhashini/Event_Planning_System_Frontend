9651b970ba65f1df61f43ceb8a64dcb3aaf28529
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-08-28 10:50:45 +0530

    Update customer Profile.js

--- src/components/Customer/Profile/Profile.js
+++ src/components/Customer/Profile/Profile.js
@@ -133,28 +133,6 @@ const Profile = () => {
                             </Typography>
                             <Typography
                                 component={Link}
-                                to="/about"
-                                className={classes.appBarButton}
-                                onClick={() => handleCategoryClick("About")}
-                                style={{
-                                    color: clickedCategory === "About" ? "#F50057" : "",
-                                }}
-                            >
-                                About
-                            </Typography>
-                            {/*<Typography*/}
-                            {/*    component={Link}*/}
-                            {/*    to="/service"*/}
-                            {/*    className={classes.appBarButton}*/}
-                            {/*    onClick={() => handleCategoryClick("Services")}*/}
-                            {/*    style={{*/}
-                            {/*        color: clickedCategory === "Services" ? "#F50057" : "",*/}
-                            {/*    }}*/}
-                            {/*>*/}
-                            {/*    Services*/}
-                            {/*</Typography>*/}
-                            <Typography
-                                component={Link}
                                 to="/booking"
                                 className={classes.appBarButton}
                                 onClick={() => handleCategoryClick("Bookings")}
