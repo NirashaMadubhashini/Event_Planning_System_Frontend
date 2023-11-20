c01d1c6539c6435dfa61473d4f1cab89ffd9a619
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-11-20 22:15:18 +0530

    Update LandingPage.js

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,36 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import {Button, Menu,MenuItem} from '@material-ui/core';
+import { Button, Menu, MenuItem } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     landingPage: {
-        background: `url("https://cfw.rabbitloader.xyz/eyJjIjp0cnVlLCJoIjoicGlua2Nhdmlhci5jb20uYXUiLCJ2IjoxOTIwMDgwMzQxLCJpIjoiODFjODBjMjItMzZmMC00OWI5LTRlYmYtNjA4NGVmOTllNDAwIn0/wp-content/uploads/2023/03/event-decorations-beautiful-corporate-theme-party-1080x810.jpg")`,
+        background: 'url("https://www.datocms-assets.com/9759/1590743134-18th-black-white-birthday-party-kent-013.jpg") no-repeat center center',
         backgroundSize: 'cover',
-        // backgroundPosition: 'center',
-        height: '97vh',
-        width: '209vh',
+        margin:'-24px',
+        padding: 0,
+        width: '100vw',
+        height: '105vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        color: 'white',
+        color: '#fff',
         textAlign: 'center',
-        overflowX: 'hidden', // Prevent horizontal scrollbar
-        overflowY: 'hidden', // Prevent horizontal scrollbar
+        overflow: 'hidden',
     },
     websiteName: {
         fontSize: '3rem',
         fontWeight: 'bold',
         marginBottom: '1rem',
+        color: '#FFFFFF', // White for contrast
+        textShadow: '2px 2px 4px #000000', // Optional shadow for better readability
     },
     heading: {
         fontSize: '4rem',
+        fontWeight: 'bold',
         marginBottom: '1rem',
+        color: '#F1E4F3', // A light purple that matches the image
+        textShadow: '2px 2px 4px #000000',
     },
     subHeading: {
         fontSize: '1.5rem',
+        fontWeight: 'normal', // Less prominent than the main heading
         marginBottom: '2rem',
+        color: '#F8E8F8', // A lighter shade for subheading
+        textShadow: '1px 1px 2px #000000',
     },
     buttonContainer: {
         display: 'flex',
@@ -44,6 +52,14 @@ const useStyles = makeStyles((theme) => ({
                 transform: 'scale(1.05)',
             },
         },
+        '& .MuiButton-containedPrimary': {
+            backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent white
+            color: '#303F9F', // Purple text
+        },
+        '& .MuiButton-containedSecondary': {
+            backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent white
+            color: '#F50057', // Pink text
+        },
     },
 }));
 
@@ -51,6 +67,20 @@ const LandingPage = () => {
     const classes = useStyles();
     const history = useHistory();
     const [anchorEl, setAnchorEl] = useState(null);
+
+    useEffect(() => {
+        // Apply styles to body when component mounts
+        document.body.style.margin = '0';
+        document.body.style.padding = '0';
+        document.body.style.overflow = 'hidden'; // Prevent scrolling on the entire body
+
+        // Reset body styles when component unmounts
+        return () => {
+            document.body.style.margin = '';
+            document.body.style.padding = '';
+            document.body.style.overflow = '';
+        };
+    }, []);
 
     const handleGetStartedClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -72,9 +102,6 @@ const LandingPage = () => {
         history.push(routePath);
         handleGetStartedClose();
     };
-    // const handleGetStarted = (role) => {
-    //     history.push(`/get-started/${role}`);
-    // };
 
     return (
         <div className={classes.landingPage}>
@@ -89,25 +116,23 @@ const LandingPage = () => {
                 >
                     About Us
                 </Button>
-                <div className={classes.buttonContainer}>
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={handleGetStartedClick}
-                    >
-                        Get Started !
-                    </Button>
-                    <Menu
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleGetStartedClose}
-                    >
-                        <MenuItem onClick={() => handleRoleSelection('admin')}>As an Admin</MenuItem>
-                        <MenuItem onClick={() => handleRoleSelection('customer')}>As a Customer</MenuItem>
-                        <MenuItem onClick={() => handleRoleSelection('vendor')}>As a Vendor</MenuItem>
-                    </Menu>
-                </div>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleGetStartedClick}
+                >
+                    Get Started !
+                </Button>
+                <Menu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleGetStartedClose}
+                >
+                    <MenuItem onClick={() => handleRoleSelection('admin')}>As an Admin</MenuItem>
+                    <MenuItem onClick={() => handleRoleSelection('customer')}>As a Customer</MenuItem>
+                    <MenuItem onClick={() => handleRoleSelection('vendor')}>As a Vendor</MenuItem>
+                </Menu>
             </div>
         </div>
     );
