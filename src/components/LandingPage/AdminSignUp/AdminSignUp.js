219d47f6ea27be05a90c3204bba6b3bb8322d4b3
NirashaMadubhashini <nirashamadu0729@gmail.com>
2024-01-06 00:40:32 +0530

    Update AdminSignUp.js

--- src/components/LandingPage/AdminSignUp/AdminSignUp.js
+++ src/components/LandingPage/AdminSignUp/AdminSignUp.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { AppBar, Typography, Toolbar, Avatar, Button, Paper, Grid, Container } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { AppBar, Typography, Toolbar, Avatar, Button, Paper, Grid, Container, Snackbar } from '@material-ui/core';
+import Alert from '@mui/material/Alert';
 import { Link, useHistory } from 'react-router-dom';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
@@ -13,29 +15,89 @@ const AdminSignUp = () => {
     const history = useHistory();
 
     const [form, setForm] = useState({
-        email: '', password: ''
+        nic: '', name: '', address: '', contactNo: '', email: '', type: '', password: '', confirmPassword: ''
     });
+    const [isSignup, setIsSignup] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+    const userRole = useSelector((state) => state.adminReducer.userRole);
+    const [error, setError] = useState('');
+
+    // New state variables for Snackbar
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [snackbarSeverity, setSnackbarSeverity] = useState('info');
 
     const handleShowPassword = () => setShowPassword(!showPassword);
+
+    const switchMode = () => {
+        setForm({
+            nic: '', name: '', address: '', contactNo: '', email: '', type: '', password: '', confirmPassword: ''
+        });
+        setIsSignup(!isSignup);
+        setShowPassword(false);
+    };
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
-
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setError(''); // Clear any existing errors
+
+        if (isSignup && form.password !== form.confirmPassword) {
+            setError("Passwords don't match.");
+            setSnackbarMessage("Passwords don't match.");
+            setSnackbarSeverity('error');
+            setSnackbarOpen(true);
+            return;
+        }
+
         try {
-            const response = await AuthService.login(form.email,form.password);
-            history.push('/adminDashboard');
-
-        }catch (e) {
-            console.error('Login failed:', e.message);
-
+            if (isSignup) {
+                const userData = {
+                    username: form.name,
+                    password: form.password,
+                    roles: [userRole]
+                };
+                const response = await AuthService.register(userData);
+                setIsSignup(false);
+                setError('');
+                setSnackbarMessage('Registration successful. Please log in.');
+                setSnackbarSeverity('success');
+                setSnackbarOpen(true);
+            } else {
+                const response = await AuthService.login(form.name, form.password);
+                if (response.token) {
+                    localStorage.setItem('jwtToken', response.token);
+                    history.push('/adminDashboard');
+                    setError('');
+                    setSnackbarMessage('Login successful');
+                    setSnackbarSeverity('success');
+                    setSnackbarOpen(true);
+                } else {
+                    setError(response.message || 'Login failed');
+                    setSnackbarMessage(response.message || 'Login failed');
+                    setSnackbarSeverity('error');
+                    setSnackbarOpen(true);
+                }
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || 'Authentication failed';
+            setError(errorMessage);
+            setSnackbarMessage(errorMessage);
+            setSnackbarSeverity('error');
+            setSnackbarOpen(true);
         }
     };
+    const handleBlur = () => {
+        setError(''); // Clear error message
+    };
 
+    const containerStyle = {
+        overflowY: 'auto', // make it scrollable
+        maxHeight: isSignup ? 'none' : '90vh', // adjust max height based on isSignup state
+    };
 
     return (
         <>
@@ -54,17 +116,37 @@ const AdminSignUp = () => {
                     <Avatar className={classes.avatar}>
                         <LockOutlinedIcon />
                     </Avatar>
-                    <Typography component="h1" variant="h5">Sign in</Typography>
+                    <Typography component="h1" variant="h5">
+                        {isSignup ? 'Sign up' : 'Sign in'}
+                    </Typography>
                     <form className={classes.form} onSubmit={handleSubmit}>
                         <Grid container spacing={2}>
-                            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                            {isSignup && (
+                                <>
+                                    <Input name="nic" label="NIC" handleChange={handleChange} autoFocus half />
+                                    <Input name="address" label="Address" handleChange={handleChange} half />
+                                    <Input name="contactNo" label="Contact No" handleChange={handleChange} />
+                                    <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                                </>
+                            )}
+                            <Input
+                                name="name"
+                                label="Name"
+                                handleChange={handleChange}
+                                handleBlur={handleBlur}
+                            />
                             <Input
                                 name="password"
                                 label="Password"
                                 handleChange={handleChange}
                                 type={showPassword ? 'text' : 'password'}
                                 handleShowPassword={handleShowPassword}
+                                handleBlur={handleBlur}
                             />
+                            {isSignup && (
+                                <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />
+                            )}
+                            {!isSignup && error && <Typography color="error">{error}</Typography>}
                         </Grid>
                         <Button
                             type="submit"
@@ -72,15 +154,32 @@ const AdminSignUp = () => {
                             variant="contained"
                             color="primary"
                             className={classes.submit}
-                            component={Link}
-                            to="/adminDashboard"
                         >
-                            Sign In
+                            {isSignup ? 'Sign Up' : 'Sign In'}
                         </Button>
 
+                        {/*<Grid container justify="center">*/}
+                        {/*    /!*<Grid item>*!/*/}
+                        {/*    /!*    <Typography>*!/*/}
+                        {/*    /!*        {isSignup ? 'Already have an account?' : "Don't have an account?"}*!/*/}
+                        {/*    /!*    </Typography>*!/*/}
+                        {/*    /!*</Grid>*!/*/}
+                        {/*    <Grid item>*/}
+                        {/*        <Button className={classes.button} >*/}
+                        {/*            Sign in*/}
+                        {/*        </Button>*/}
+                        {/*    </Grid>*/}
+                        {/*</Grid>*/}
                     </form>
                 </Paper>
             </Container>
+
+            {/* Snackbar for displaying alerts */}
+            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={() => setSnackbarOpen(false)}>
+                <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity} sx={{ width: '100%' }}>
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
         </>
     );
 };
